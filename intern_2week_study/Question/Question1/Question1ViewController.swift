f33500e3f63a2e1425d1f4211b4eb02c7a0e1941
KoenHan <sliver1988324@gmail.com>
2020-08-12 17:21:47 +0900

    課題1-3を実装

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -6,19 +6,26 @@ final class Question1ViewController: UIViewController {
     @IBOutlet weak var addTextButton: UIButton!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var clearTextButton: UIButton!
+    @IBOutlet weak var textWarning: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         textField.text = ""
         textView.text = ""
+        textWarning.isHidden = true
     }
     
     @IBAction func addText(_ sender: Any) {
         guard let textFieldValue = textField.text else {
             return
         }
-        textView.text += textFieldValue + "\n"
-        textField.text = ""
+        if textFieldValue == "" {
+            textWarning.isHidden = false
+        } else {
+            textWarning.isHidden = true
+            textView.text += textFieldValue + "\n"
+            textField.text = ""
+        }
     }
     
     @IBAction func clearText(_ sender: Any) {

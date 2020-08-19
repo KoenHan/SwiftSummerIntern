26dfb06e1545d8a217d7cf57e6dc121a034cdd37
KoenHan <sliver1988324@gmail.com>
2020-08-19 14:00:39 +0900

    検索文字が空の時の処理を追加

--- intern_2week_study/Question/Question3/Search/SearchViewController.swift
+++ intern_2week_study/Question/Question3/Search/SearchViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 final class SearchViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var warningLabel: UILabel!
     
     @IBAction func searchButtonTapped(_ sender: UIButton) {
         searchArticles()
@@ -10,24 +11,29 @@ final class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        warningLabel.isHidden = true
     }
 
     private func searchArticles() {
         searchButton.isEnabled = false
         
-        guard let text = textField.text, !text.isEmpty else { return }
+        guard let text = textField.text, !text.isEmpty else {
+            warningLabel.isHidden = false
+            searchButton.isEnabled = true
+            return
+        }
         
         APIClient.fetchArticles(keyword: text) { [weak self] result in
-//            guard let self = self else { return }
+            guard let self = self else { return }
             DispatchQueue.main.sync {
                 switch result {
                 case .success(let articles):
-                    self?.showArticleListScreen(articles)
+                    self.showArticleListScreen(articles)
                 case .failure(let error):
                     let alert = UIAlertController.createErrorAlert(error)
-                    self?.present(alert, animated: true)
+                    self.present(alert, animated: true)
                 }
-                self?.searchButton.isEnabled = true
+                self.searchButton.isEnabled = true
             }
         }
     }

085da7d99720c9e88a4e5e37c91ddeaa08b77fb6
KoenHan <sliver1988324@gmail.com>
2020-08-19 16:31:17 +0900

    一時中断

--- intern_2week_study/Question/Question3/ArticleList/ArticleListCell.swift
+++ intern_2week_study/Question/Question3/ArticleList/ArticleListCell.swift
@@ -4,12 +4,19 @@ import UIKit
 class ArticleListCell: UITableViewCell {
     @IBOutlet weak var userImageView: UIImageView!
     @IBOutlet weak var titleTextLabel: UILabel!
-    @IBOutlet weak var discriptionTextLabel: UILabel!
+    @IBOutlet weak var userNameTextLabel: UILabel!
+    @IBOutlet weak var lgtmTextLabel: UILabel!
+    @IBOutlet weak var tagsTextLabel: UILabel!
     
     public func setArea(_ article: Article) {
         titleTextLabel.text = article.title
-        discriptionTextLabel.text = "LGTM: " +  String(article.likesCount)
+        lgtmTextLabel.text = "LGTM: " +  String(article.likesCount)
+        userNameTextLabel.text = article.user?.name
         
+        tagsTextLabel.text = "Tags: "
+        for tag in article.tags {
+            tagsTextLabel.text? += tag.name + " "
+        }
         if let urlString = article.user?.profileImageUrl,
             let url = URL(string: urlString) {
             Nuke.loadImage(with: url, into: userImageView)

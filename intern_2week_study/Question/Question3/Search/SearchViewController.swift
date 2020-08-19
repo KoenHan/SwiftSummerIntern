00b2314b6dde4bbedabae0e98aa9189d63a2b245
KoenHan <sliver1988324@gmail.com>
2020-08-19 16:00:33 +0900

    検索ボタンのクリック感を追加

--- intern_2week_study/Question/Question3/Search/SearchViewController.swift
+++ intern_2week_study/Question/Question3/Search/SearchViewController.swift
@@ -49,6 +49,22 @@ final class SearchViewController: UIViewController {
         searchButton.backgroundColor = UIColor(red: 0.2, green: 0.6, blue: 1.0, alpha: 1.0)
         searchButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
         searchButton.layer.cornerRadius = 10
+        searchButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        searchButton.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        searchButton.layer.shadowOpacity = 1.0
+        searchButton.layer.shadowRadius = 0.0
     }
     
+    @IBAction func pushSearchButton(_ sender: Any) {
+        searchButton.backgroundColor = UIColor(red: 0.137, green: 0.576, blue: 0.976, alpha: 1.0)
+        searchButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        searchButton.layer.cornerRadius = 10
+        searchButton.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        searchButton.layer.shadowOpacity = 0.0
+    }
+    
+    @IBAction func releaseSearchButton(_ sender: Any) {
+        setSearchButtonDesign()
+        
+    }
 }

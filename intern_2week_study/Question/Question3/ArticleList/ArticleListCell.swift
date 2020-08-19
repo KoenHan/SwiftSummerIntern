471ab3c213d1e6f2d57e887664c76ad64041a133
KoenHan <sliver1988324@gmail.com>
2020-08-19 17:34:23 +0900

    タグとユーザーネームを表示するようにした

--- intern_2week_study/Question/Question3/ArticleList/ArticleListCell.swift
+++ intern_2week_study/Question/Question3/ArticleList/ArticleListCell.swift
@@ -4,19 +4,19 @@ import UIKit
 class ArticleListCell: UITableViewCell {
     @IBOutlet weak var userImageView: UIImageView!
     @IBOutlet weak var titleTextLabel: UILabel!
-//    @IBOutlet weak var userNameTextLabel: UILabel!
+    @IBOutlet weak var userNameTextLabel: UILabel!
 //    @IBOutlet weak var lgtmTextLabel: UILabel!
     @IBOutlet weak var tagsTextLabel: UILabel!
     
     public func setArea(_ article: Article) {
         titleTextLabel.text = article.title
-//        lgtmTextLabel.text = "LGTM: " +  String(article.likesCount)
-//        userNameTextLabel.text = article.user?.name
+        userNameTextLabel.text = article.user?.name
         
         tagsTextLabel.text = "Tags: "
         for tag in article.tags {
             tagsTextLabel.text? += String(tag.name) + " "
         }
+
         if let urlString = article.user?.profileImageUrl,
             let url = URL(string: urlString) {
             Nuke.loadImage(with: url, into: userImageView)
